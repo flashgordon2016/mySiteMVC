d13cb903d7b571230106e69730ee02e342f65e6b
Matt Morse <matt.morse1988@gmail.com>
2018-08-31 12:24:15 -0800

    Make page menu section active when clicked

--- MySiteMVC/wwwroot/js/site.js
+++ MySiteMVC/wwwroot/js/site.js
@@ -7,46 +7,12 @@ $('.dropdown-menu button').click(function (e) {
     e.stopPropagation();
 });
 
-
 /* Tooltips affect performance and are thus opt-in. They are initialized here */
 $(function () {
     $('[data-toggle="tooltip"]').tooltip();
 });
 
-/* Begin credit: https://css-tricks.com/snippets/jquery/smooth-scrolling/ */
-// Select all links with hashes
-$('a[href*="#"]')
-    // Remove links that don't actually link to anything
-    .not('[href="#"]')
-    .not('[href="#0"]')
-    .click(function (event) {
-        // On-page links
-        if (location.pathname.replace(/^\//, '') === this.pathname.replace(/^\//, '')
-            &&
-            location.hostname === this.hostname) {
-            // Figure out element to scroll to
-            var target = $(this.hash);
-            target = target.length ? target : $('[name=' + this.hash.slice(1) + ']');
-            // Does a scroll target exist?
-            if (target.length) {
-                $('html, body').animate({
-                    scrollTop: target.offset().top
-                }, 1000, function () {
-                    // Callback after animation
-                    // Must change focus!
-                    var $target = $(target);
-                    $target.focus();
-                    if ($target.is(":focus")) { // Checking if the target was focused
-                        return false;
-                    } else {
-                        $target.attr('tabindex', '-1'); // Adding tabindex for elements not focusable
-                        $target.focus(); // Set focus again
-                    }
-                });
-            }
-        }
-    });
-/* End credit: https://css-tricks.com/snippets/jquery/smooth-scrolling/ */
+
 
 
 /* Credit: https://stackoverflow.com/questions/25455567/how-can-i-select-a-paragraph-with-javascript-on-click/25456308#25456308 */
@@ -98,8 +64,6 @@ function togglePageMenu() {
             //change text from 'expand' to 'collapse'
             $('.collapseBarText .swappedText').text('collapse');
         });
-
-
     }
 }
 
@@ -126,4 +90,42 @@ $(document).ready(function () {
     });
 });
 
+/* Begin credit: https://css-tricks.com/snippets/jquery/smooth-scrolling/ */
+// Select all links with hashes
+$('a[href*="#"]')
+    // Remove links that don't actually link to anything
+    .not('[href="#"]')
+    .not('[href="#0"]')
+    .click(function (event) {
+        // On-page links
+        if (location.pathname.replace(/^\//, '') === this.pathname.replace(/^\//, '')
+            &&
+            location.hostname === this.hostname) {
+            // Figure out element to scroll to
+            var target = $(this.hash);
+            var id = $(this).attr('href');
+            target = target.length ? target : $('[name=' + this.hash.slice(1) + ']');
+            // Does a scroll target exist?
+            if (target.length) {
+                $('html, body').animate({
+                    scrollTop: target.offset().top
+                }, 1000, function () {
+                    // Callback after animation
+                    // Must change focus!
+                    var $target = $(target);
+                    $target.focus();
+                    if ($target.is(":focus")) { // Checking if the target was focused
+                        //Set target item to active
+                        $('#pageMenu nav ol li').removeClass('active');
+                        $('#pageMenu nav ol li a[href=\'' + id + '\']').parent().addClass('active');
+                        return false;
+                    } else {
+                        $target.attr('tabindex', '-1'); // Adding tabindex for elements not focusable
+                        $target.focus(); // Set focus again
+                    }
+                });
+            }
+        }
+    });
+/* End credit: https://css-tricks.com/snippets/jquery/smooth-scrolling/ */
 
